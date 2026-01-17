915bb9b2177de408b66256d45a7ccda1bcba019a
sbx_user1051 <sbx_user1051@169.254.4.31>
2026-01-17 22:57:48 +0000

    Sat Jan 17 22:57:48 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -10,23 +10,23 @@ def test_enqueue_size_dequeue_flow() -> None:
         call_dequeue().expect("companies_house", 1),
     ])
 
-# def test_rule_of_3() -> None:
-#     run_queue([
-#         call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
-#         call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(1),
-#         call_enqueue("id_verification", 1, iso_ts(delta_minutes=0)).expect(1),
-#         call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(1),
-#         call_size().expect(4),
-#         call_dequeue().expect("companies_house", 1),
-#         call_dequeue().expect("id_verification", 1),
-#         call_dequeue().expect("bank_statements", 1),
-#         call_dequeue().expect("bank_statements", 2),
-#     ])
+def test_rule_of_3() -> None:
+    run_queue([
+        call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(2),
+        call_enqueue("id_verification", 1, iso_ts(delta_minutes=0)).expect(3),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(4),
+        call_size().expect(4),
+        call_dequeue().expect("companies_house", 1),
+        call_dequeue().expect("id_verification", 1),
+        call_dequeue().expect("bank_statements", 1),
+        call_dequeue().expect("bank_statements", 2),
+    ])
 
 def test_timestamp_order() -> None:
     run_queue([
         call_enqueue("bank_statements", 1, iso_ts(delta_minutes=5)).expect(1),
-        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(2),
         call_size().expect(2),
         call_dequeue().expect("bank_statements", 2),
         call_dequeue().expect("bank_statements", 1),

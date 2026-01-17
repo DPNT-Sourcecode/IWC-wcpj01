aba20f0f3ceca0536b33cc4f3c31e8ac9050adc0
sbx_user1051 <sbx_user1051@169.254.4.31>
2026-01-17 22:45:48 +0000

    Sat Jan 17 22:45:48 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -9,3 +9,57 @@ def test_enqueue_size_dequeue_flow() -> None:
         call_size().expect(1),
         call_dequeue().expect("companies_house", 1),
     ])
+
+
+def test_rule_of_3() -> None:
+    run_queue([
+        call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("id_verification", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_size().expect(4),
+        call_dequeue().expect("companies_house", 1),
+        call_dequeue().expect("id_verification", 1),
+        call_dequeue().expect("bank_statements", 1),
+        call_dequeue().expect("bank_statements", 2),
+    ])
+
+
+"""
+Example #1 - Rule of 3:
+--------
+The following operations show how the Rule of 3 affects queue priority.
+
+1. Enqueue: user_id=1, provider="companies_house",   timestamp='2025-10-20 12:00:00'  -> 1 (queue size)  
+2. Enqueue: user_id=2, provider="bank_statements",   timestamp='2025-10-20 12:00:00'  -> 2 (queue size)  
+3. Enqueue: user_id=1, provider="id_verification",   timestamp='2025-10-20 12:00:00'  -> 3 (queue size)  
+4. Enqueue: user_id=1, provider="bank_statements",   timestamp='2025-10-20 12:00:00'  -> 4 (queue size)  
+5. Dequeue -> {"user_id": 1, "provider": "companies_house"}  
+6. Dequeue -> {"user_id": 1, "provider": "id_verification"}  
+7. Dequeue -> {"user_id": 1, "provider": "bank_statements"}  
+8. Dequeue -> {"user_id": 2, "provider": "bank_statements"}  
+
+Once user 1 reaches 3 tasks, all of their jobs are moved ahead of user 2's, regardless of the
+original enqueue order.
+
+
+Example #2 - Timestamp Ordering:
+--------
+The following operations show how the order of tasks is determined by their timestamp.
+
+1. Enqueue: user_id=1, provider="bank_statements", timestamp='2025-10-20 12:05:00'  -> 1 (queue size)
+2. Enqueue: user_id=2, provider="bank_statements", timestamp='2025-10-20 12:00:00'  -> 2 (queue size)
+3. Dequeue -> {"user_id": 2, "provider": "bank_statements"}  
+4. Dequeue -> {"user_id": 1, "provider": "bank_statements"}  
+
+
+Example #3 - Dependency Resolution:
+--------
+The following operations show that the when a task is enqueued, all its dependencies are also added.
+
+1. Enqueue: user_id=1, provider="credit_check", timestamp='2025-10-20 12:00:00'  -> 2 (queue size)
+2. Dequeue -> {"user_id": 1, "provider": "companies_house"}  
+3. Dequeue -> {"user_id": 1, "provider": "credit_check"}
+
+
+"""

7a562ee2867f30139c598d6117239be4c4adb21d
sbx_user1051 <sbx_user1051@169.254.26.43>
2026-01-18 18:08:30 +0000

    Sun Jan 18 18:08:30 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -95,6 +95,15 @@ def test_age_calculation_single_element() -> None:
         call_age().expect(0),
     ])
 
+def test_age_calculation() -> None:
+    run_queue([
+        call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=1)).expect(2),
+        call_enqueue("id_verification", 1, iso_ts(delta_minutes=2)).expect(3),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=5)).expect(4),
+        call_size().expect(4),
+        call_age().expect(300),  # 5 minutes in seconds
+    ])
 
 """
 New Requirement:
@@ -169,3 +178,4 @@ The following operations show that the when a task is enqueued, all its dependen
 
 
 """
+

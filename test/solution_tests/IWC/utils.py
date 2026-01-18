cb9280d47f05f7f321b16dcd8dc55b66fc272601
sbx_user1051 <sbx_user1051@169.254.26.43>
2026-01-18 17:59:30 +0000

    Sun Jan 18 17:59:30 UTC 2026

--- test/solution_tests/IWC/utils.py
+++ test/solution_tests/IWC/utils.py
@@ -41,6 +41,10 @@ def call_size() -> QueueActionBuilder:
     return QueueActionBuilder("size")
 
 
+def call_age() -> QueueActionBuilder:
+    return QueueActionBuilder("age")
+
+
 def call_dequeue() -> QueueActionBuilder:
     return QueueActionBuilder(
         "dequeue",
@@ -72,3 +76,4 @@ def run_queue(actions: Iterable[dict[str, Any]]) -> None:
 
 
 __all__ = ["iso_ts", "call_enqueue", "call_size", "call_dequeue", "run_queue"]
+
